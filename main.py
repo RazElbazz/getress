9193af8d810fd211511eed18e086f5c9d60d47a6
Raz Elbaz <razelbaz123@gmail.com>
2025-05-22 02:38:02 +0300

    Simple listen fix

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import socket
 import threading
+from typing import Tuple
 
-def is_port_available(port: int) -> bool:
+SocketAddress = Tuple[str, int]
+
+
+def is_port_available(address: SocketAddress) -> bool:
+    """
+        Function that returns whether or not
+        the given port is in use.
     """
 
-    """
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex(('127.0.0.1', port))
-
-    return result == 0
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        result = sock.connect_ex(address)
+        return result != 0
 
 
 def handle_client(client_socket):
@@ -23,22 +27,26 @@ class GetressApp():
     def __init__(self):
         pass
 
-    def listen(ip = None, port = None):
-        if port is None:
-            port = 3000
-            while not is_port_available(port):
-                port += 1
-        if not is_port_available(port):
-            raise Exception(f"Port {port} is not available.")
-
+    def listen(self, ip = None, port = None):
         if ip is None:
             ip = "0.0.0.0"
+        if port is None:
+            port = 3000
+            while not is_port_available((ip, port)):
+                print(f"Port {port} is not available")
+                port += 1
+        if not is_port_available((ip, port)):
+            raise Exception(f"Port {port} is not available.")
+
+        print(f"Making TCP socket")
 
         # make TCP socket
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
             # bind socket to ip and port
-            server_sock.bind(address=(ip, port))
+            server_sock.bind((ip, port))
             server_sock.listen()
+
+            print(f"Listening on {server_sock.getsockname()}")
 
             # start listening to clients
             while True:
